a4a9d1c0b7393473ab80cad5f620faea29aac784
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-11-03 21:38:41 +0500

    query parameters

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ def index():
 @app.get('/about')
 def about():
     return {'data': 'i am about'}
+
+
+@app.get('/list')
+# query has limit in it e.g. domain.com/list?limit=10
+def get_list(limit: int = 10, published: bool = True):
+    if published:
+        return {
+            'data': f'{limit} list items but published only'
+        }
+    else:
+        return {
+            'data': f'{limit} list items only (not published excluded)'
+        }
