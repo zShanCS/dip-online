107c393802ae7fc6a2764205392a4c1f0b4cc86a
zShan Ahmad <mahmad.zeeshan72@gmail.com>
2021-11-09 21:34:52 +0500

    added fake delay to get_neighbors

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import random
+import time
 
 from starlette.requests import Request
 app = FastAPI()
 
 
 async def get_neighbors(limit: int):
+    time.sleep(5)
     list = []
     for i in range(limit):
         list.append(random.randint(0, 10000))
@@ -20,7 +22,7 @@ templates = Jinja2Templates(directory='templates')
 @app.post("/uploadfiles/", response_class=HTMLResponse)
 async def create_upload_files(request: Request, resultLimit: int = Form(...), file: UploadFile = File(...)):
     res = await get_neighbors(resultLimit)
-    return templates.TemplateResponse('first.html',
+    return templates.TemplateResponse('results.html',
                                       {"request": request,
                                        "list": res,
                                        "limit": resultLimit}
