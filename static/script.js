d277aef1d05b192286e36495eff8d5419eb23345
zShan <73174295+zShanCS@users.noreply.github.com>
2021-11-22 10:10:50 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,18 +1,29 @@
 console.log('hello world')
-const send = document.querySelector('button')[0]
+const send = document.querySelector('#send')
 const form = document.querySelector('#myform')
 const show = document.querySelector('#show')
+const loadmore = document.querySelector('#loadmore')
+let page = 0;
 console.log(form);
-form.addEventListener('submit', async(e) => {
-    e.preventDefault();
-    console.log('someone tried to submit the form')
+
+const load_images = async ()=>{
+    page = page+1
+    const body = new FormData(form)
+    body.append('resultLimit',page*20)
     const res = await fetch('/uploadFile', {
         method: 'POST',
-        body: new FormData(form)
+        body
     })
     const images = await res.json();
-    data = images.map(i => `<img src="/images/${i}.png" alt=`image ${i}` />`)
-
-    console.log(data)
+    
+    data = images.map(i => `<img src="/images/${i}.png" alt='images ${i}' />`)
     show.innerHTML = data.join('')
+    loadmore.hidden = false
+}
+form.addEventListener('submit', async(e) => {
+    console.log(form)
+    e.preventDefault();
+    console.log('someone tried to submit the form')
+    load_images()
 });
+loadmore.addEventListener('click',load_images)
